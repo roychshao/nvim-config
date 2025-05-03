26d472079325aef1d92cbca275c0b3d69b5194c1
roychshao <roych.shao@gmail.com>
2025-05-04 05:07:25 +0800

    [feat] from bufferline to tabby.nvim, from kaganawa to kaganawa-paper

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -24,9 +24,73 @@ return {
     --         vim.cmd [[colorscheme gruvbox-material]]
     --     end
     -- },
+    -- {
+    --     "rebelot/kanagawa.nvim",
+    --     name = "kanagawa",
+    --     lazy = false,
+    --     priority = 1000,
+    --     dependencies = {
+    --         "nvim-lualine/lualine.nvim",
+    --         "nvim-tree/nvim-web-devicons",
+    --         "utilyre/barbecue.nvim",
+    --         "SmiteshP/nvim-navic",
+    --     },
+    --     config = function()
+    --         require('kanagawa').setup({
+    --             compile = false,             -- enable compiling the colorscheme
+    --             undercurl = true,            -- enable undercurls
+    --             commentStyle = { italic = true },
+    --             functionStyle = {},
+    --             keywordStyle = { italic = true},
+    --             statementStyle = { bold = true },
+    --             typeStyle = {},
+    --             transparent = false,         -- do not set background color
+    --             dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    --             terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    --             colors = {                   -- add/modify theme and palette colors
+    --                 palette = {},
+    --                 theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    --             },
+    --             overrides = function(colors)
+    --                 local theme = colors.theme
+    --                 return {
+    --                     NormalFloat = { bg = "none" },
+    --                     FloatBorder = { bg = "none" },
+    --                     FloatTitle = { bg = "none" },
+    --
+    --                     -- Save an hlgroup with dark background and dimmed foreground
+    --                     -- so that you can use it where your still want darker windows.
+    --                     -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+    --                     NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+    --
+    --                     -- Popular plugins that open floats will link to NormalFloat by default;
+    --                     -- set their background accordingly if you wish to keep them dark and borderless
+    --                     LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+    --                     MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+    --                 }
+    --             end,
+    --             theme = "all",               -- Load all theme
+    --             background = {               -- map the value of 'background' option to a theme
+    --                 dark = "wave",           -- try "dragon" !
+    --                 light = "lotus"
+    --             },
+    --         })
+    --         require('lualine').setup({
+    --             options = {
+    --                 theme = 'kanagawa'
+    --             },
+    --         })
+    --         require('barbecue').setup {
+    --             theme = 'kanagawa'
+    --         }
+    --         vim.g.gruvbox_material_enable_italic = true
+    --         vim.g.gruvbox_material_foreground = 'mix'
+    --         vim.cmd [[colorscheme kanagawa]]
+    --     end
+    -- },
     {
-        "rebelot/kanagawa.nvim",
-        name = "kanagawa",
+        "thesimonho/kanagawa-paper.nvim",
+        name = 'kanagawa-paper',
         lazy = false,
         priority = 1000,
         dependencies = {
@@ -36,56 +100,18 @@ return {
             "SmiteshP/nvim-navic",
         },
         config = function()
-            require('kanagawa').setup({
-                compile = false,             -- enable compiling the colorscheme
-                undercurl = true,            -- enable undercurls
-                commentStyle = { italic = true },
-                functionStyle = {},
-                keywordStyle = { italic = true},
-                statementStyle = { bold = true },
-                typeStyle = {},
-                transparent = false,         -- do not set background color
-                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-                colors = {                   -- add/modify theme and palette colors
-                    palette = {},
-                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-                },
-                overrides = function(colors)
-                    local theme = colors.theme
-                    return {
-                        NormalFloat = { bg = "none" },
-                        FloatBorder = { bg = "none" },
-                        FloatTitle = { bg = "none" },
-
-                        -- Save an hlgroup with dark background and dimmed foreground
-                        -- so that you can use it where your still want darker windows.
-                        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-                        -- Popular plugins that open floats will link to NormalFloat by default;
-                        -- set their background accordingly if you wish to keep them dark and borderless
-                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                    }
-                end,
-                theme = "all",               -- Load all theme
-                background = {               -- map the value of 'background' option to a theme
-                    dark = "wave",           -- try "dragon" !
-                    light = "lotus"
-                },
-            })
+            local kanagawa_paper = require("lualine.themes.kanagawa-paper-ink")
             require('lualine').setup({
                 options = {
-                    theme = 'kanagawa'
+                    theme = kanagawa_paper
                 },
             })
             require('barbecue').setup {
-                theme = 'kanagawa'
+                theme = 'kanagawa-paper'
             }
             vim.g.gruvbox_material_enable_italic = true
             vim.g.gruvbox_material_foreground = 'mix'
-            vim.cmd [[colorscheme kanagawa]]
+            vim.cmd [[colorscheme kanagawa-paper]]
         end
-    }
+    },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,118 +1,89 @@
 return {
-	{
-		"akinsho/bufferline.nvim",
-		event = "VeryLazy",
-		-- commit = "0b2fd86",
-		config = function()
-			vim.keymap.set("n", "gT", ":BufferLineCyclePrev<CR>", { desc = "BufferLine toggle to previous one" })
-			vim.keymap.set("n", "gt", ":BufferLineCycleNext<CR>", { desc = "BufferLine toggle to next one" })
-			require("bufferline").setup({
-				options = {
-					separator_style = "slant",
-				},
-				highlights = {
-					buffer_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					separator_selected = {
-						fg = "bg",
-						bg = "fg",
-					},
-					indicator_selected = {
-						fg = "bg",
-						bg = "fg",
-					},
-					pick_selected = {
-						fg = "bg",
-						bg = "fg",
-					},
-					close_button_selected = {
-						fg = "bg",
-						bg = "fg",
-					},
-					numbers_selected = {
-						fg = "bg",
-						bg = "fg",
-					},
-					diagnostic_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					hint_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					hint_diagnostic_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					info_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					info_diagnostic_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					warning_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					warning_diagnostic_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					error_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					error_diagnostic_selected = {
-						fg = "bg",
-						bg = "fg",
-						bold = true,
-						italic = true,
-					},
-					modified_selected = {
-						fg = "bg",
-						bg = "fg",
-					},
-					duplicate_selected = {
-						fg = "bg",
-						bg = "fg",
-						italic = true,
-					},
-				},
-			})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "VeryLazy",
-		main = "ibl",
-		opts = {},
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "VeryLazy",
-		config = true,
-	},
+    -- {
+    --     "akinsho/bufferline.nvim",
+    --     -- event = "VeryLazy",
+    --     config = function()
+    --         vim.keymap.set("n", "gT", ":BufferLineCyclePrev<CR>", { desc = "BufferLine toggle to previous one" })
+    --         vim.keymap.set("n", "gt", ":BufferLineCycleNext<CR>", { desc = "BufferLine toggle to next one" })
+    --         require("bufferline").setup {
+    --             options = {
+    --                 separator_style = "slant",
+    --             }
+    --         }
+    --     end,
+    -- },
+    {
+        'nanozuki/tabby.nvim',
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            local theme = {
+                -- this is carbonfox theme
+                fill = 'TabLineFill',
+                head = 'TabLine',
+                current_tab = 'TabLineSel',
+                tab = 'TabLine',
+                win = 'TabLine',
+                tail = 'TabLine',
+            }
+
+            require('tabby.tabline').set(function(line)
+                return {
+                    {
+                        { '  ', hl = theme.head },
+                        line.sep('', theme.head, theme.fill),
+                    },
+                    line.tabs().foreach(function(tab)
+
+                        local hl = tab.is_current() and theme.current_tab or theme.tab
+
+                        -- remove count of wins in tab with [n+] included in tab.name()
+                        local name = tab.name()
+                        local index = string.find(name, "%[%d")
+                        local tab_name = index and string.sub(name, 1, index - 1) or name
+
+                        -- indicate if any of buffers in tab have unsaved changes
+                        local modified = false
+                        local win_ids = require('tabby.module.api').get_tab_wins(tab.id)
+                        for _, win_id in ipairs(win_ids) do
+                            if pcall(vim.api.nvim_win_get_buf, win_id) then
+                                local bufid = vim.api.nvim_win_get_buf(win_id)
+                                if vim.api.nvim_buf_get_option(bufid, "modified") then
+                                    modified = true
+                                    break
+                                end
+                            end
+                        end
+
+                        return {
+                            line.sep('', hl, theme.fill),
+                            tab_name,
+                            modified and '',
+                            line.sep('', hl, theme.fill),
+                            hl = hl,
+                            margin = ' ',
+                        }
+                    end),
+                    line.spacer(),
+                    {
+                        line.sep('', theme.tail, theme.fill),
+                        { '  ', hl = theme.tail },
+                    },
+                    hl = theme.fill,
+                }
+            end)
+            vim.o.showtabline = 2
+        end,
+    },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "VeryLazy",
+        main = "ibl",
+        opts = {},
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        config = true,
+    },
 }
