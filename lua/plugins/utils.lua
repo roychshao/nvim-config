d289c653deedd7ccd59c661d939a047415f78947
roychshao <roych.shao@gmail.com>
2025-07-07 03:22:24 +0800

    [feat] beacon.nvim

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,114 +1,114 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"comment",
-					"cpp",
-					"css",
-					"csv",
-					"diff",
-					"dockerfile",
-					"git_config",
-					"git_rebase",
-					"gitattributes",
-					"gitcommit",
-					"gitignore",
-					"go",
-					"html",
-					"java",
-					"javascript",
-					"jsdoc",
-					"json",
-					"lua",
-					"luadoc",
-					"markdown",
-					"markdown_inline",
-					"python",
-					"regex",
-					"tmux",
-					"tsx",
-					"typescript",
-					"vim",
-					"vimdoc",
-					"xml",
-					"yaml",
-					"toml",
-					"solidity",
-					"jsonc",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "comment",
+                    "cpp",
+                    "css",
+                    "csv",
+                    "diff",
+                    "dockerfile",
+                    "git_config",
+                    "git_rebase",
+                    "gitattributes",
+                    "gitcommit",
+                    "gitignore",
+                    "go",
+                    "html",
+                    "java",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "lua",
+                    "luadoc",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "regex",
+                    "tmux",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "xml",
+                    "yaml",
+                    "toml",
+                    "solidity",
+                    "jsonc",
                     "groovy"
-				},
-				highlight = {
-					enable = true,
-				},
-				indent = {
-					enable = true,
-				},
-			})
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {
-			enable_check_bracket_line = false,
-		},
-		config = true,
-	},
-	{
-		"numToStr/Comment.nvim",
-		opts = {},
-		lazy = false,
-	},
-	{
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup({ "*" }, {
+                },
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {
+            enable_check_bracket_line = false,
+        },
+        config = true,
+    },
+    {
+        "numToStr/Comment.nvim",
+        opts = {},
+        lazy = false,
+    },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "*" }, {
 
-				RGB = true, -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				names = true, -- "Name" codes like Blue
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				rgb_fn = true, -- CSS rgb() and rgba() functions
-				hsl_fn = true, -- CSS hsl() and hsla() functions
-				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-				-- Available modes: foreground, background
-				mode = "background", -- Set the display mode.
-			})
-		end,
-	},
-	{
-		"hedyhli/outline.nvim",
-		config = function()
-			-- Example mapping to toggle outline
-			vim.keymap.set("n", "<F3>", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                names = true, -- "Name" codes like Blue
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+                -- Available modes: foreground, background
+                mode = "background", -- Set the display mode.
+            })
+        end,
+    },
+    {
+        "hedyhli/outline.nvim",
+        config = function()
+            -- Example mapping to toggle outline
+            vim.keymap.set("n", "<F3>", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
-			require("outline").setup({
-				outline_window = {
-					width = 20,
-				},
-				preview_window = {
-					auto_preview = true,
-				},
-			})
-		end,
-	},
-	{
-		"smoka7/hop.nvim",
-		version = "*",
-		opts = {
-			keys = "etovxqpdygfblzhckisuran",
-		},
-		config = function()
-			local hop = require("hop")
-			hop.setup()
-			vim.api.nvim_set_keymap("n", "<leader>h", ":HopWord<CR>", { noremap = true, silent = true })
-			-- vim.api.nvim_set_keymap('n', '<leader>hl', ":HopWordCurrentLine<CR>", { noremap = true, silent = true })
-		end,
-	},
+            require("outline").setup({
+                outline_window = {
+                    width = 20,
+                },
+                preview_window = {
+                    auto_preview = true,
+                },
+            })
+        end,
+    },
+    {
+        "smoka7/hop.nvim",
+        version = "*",
+        opts = {
+            keys = "etovxqpdygfblzhckisuran",
+        },
+        config = function()
+            local hop = require("hop")
+            hop.setup()
+            vim.api.nvim_set_keymap("n", "<leader>h", ":HopWord<CR>", { noremap = true, silent = true })
+            -- vim.api.nvim_set_keymap('n', '<leader>hl', ":HopWordCurrentLine<CR>", { noremap = true, silent = true })
+        end,
+    },
 }
